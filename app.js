afde1f5c80bdb798248fcaacef790f84ccfe6811
Brennan <Brennan.Martin@arrow.com>
2018-08-16 22:10:39 -0600

    updated dom

--- app.js
+++ app.js
@@ -8,3 +8,36 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.dice').style.display = 'none';
+
+function btn(){
+    // random number
+    var dice = Math.floor(Math.random() *6 ) + 1;
+
+    //display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //update the round score if the rolled number was not a 1
+
+
+}
+
+
+
+
+document.querySelector(".btn-roll").addEventListener('click', btn);
